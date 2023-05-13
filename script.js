a878c3895196cc133df367456ea854e9b75fd75e
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-05-13 17:20:48 +0530

    Completed JS code for submitting

--- script.js
+++ script.js
@@ -8,20 +8,55 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((student)=>{
+    if (student.marks<=50){
+      console.log(student);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student)=>{
+    if(student.marks<=50){
+      console.log(student);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newstudent={id:4,name:"Rakesh",age:"26",marks:45}
+  arr.push(newstudent);
+  console.log(newstudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let res=arr.filter((student)=>{
+    if(student.marks<50){
+      return false;
+    }
+    else {
+      return true;
+    }
+  })
+  console.log(res);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newarr=[
+    {id: 5, name:"Rasika", age:"26", marks:55},
+    {id: 6, name:"Neha", age:"25", marks:80},
+    {id: 7, name:"Kunal", age:"28", marks:66}
+  ];
+  let concatarr=[];
+  arr.forEach((student)=>{
+    concatarr.push(student);
+  });
+  newarr.forEach((student)=>{
+    concatarr.push(student);
+  });
+  console.log(concatarr);
 }
